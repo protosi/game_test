c4406d4cff472a8b0dccffbf4214437e77783b4f
proto_000 <proto_000@MiDtics>
2017-04-29 21:24:00 +0900

    jsp 설정함

--- src/main/java/com/midtics/controller/TestController.java
+++ src/main/java/com/midtics/controller/TestController.java
@@ -1,15 +1,17 @@
 package com.midtics.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class TestController {
 	
 	@RequestMapping(value="test")
-	public String testFunction()
+	public String testFunction(Model model)
 	{
-		return "test";
+		model.addAttribute("name", "SpringBlog from Millky");
+		return "test_hello";
 	}
 
 }
