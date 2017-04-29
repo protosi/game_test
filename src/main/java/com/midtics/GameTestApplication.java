3415f11d71fb5bd130ab2e22d33c01008efd5559
proto_000 <proto_000@MiDtics>
2017-04-29 19:37:56 +0900

    .

---
+++ src/main/java/com/midtics/GameTestApplication.java
@@ -0,0 +1,12 @@
+package com.midtics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameTestApplication.class, args);
+	}
+}
