12250a2375d4247dc927c650163e214472d09217
proto_000 <proto_000@MiDtics>
2017-06-15 00:14:55 +0900

    로그잡을 빼먹었네

--- src/main/java/com/midtics/scheduler/EcosScheduler.java
+++ src/main/java/com/midtics/scheduler/EcosScheduler.java
@@ -19,21 +19,25 @@ public class EcosScheduler  implements ApplicationListener<ContextRefreshedEvent
 	EcosStatJobComponent statJob;
 	
 	@Scheduled(cron = "0 0 0 * * *")
-    public void reportCurrentTime() {
-		//Thread t = new Thread(bokUploader);
-		//t.start();
-		
+    public void logJob() 
+	{
+		Thread t = new Thread(bokUploader);
+		t.start();
+    }
+	
+	@Scheduled(cron = "0 0 0 * * *")
+    public void statJob() 
+	{	
 		Thread t1 = new Thread(statJob);
 		t1.start();
-		
-		
-		
     }
 	
 	@Override
 	public void onApplicationEvent(ContextRefreshedEvent arg0) {
 		// TODO Auto-generated method stub
-		reportCurrentTime();
+		logJob();
+		
+		statJob();
 		
 	}
 
